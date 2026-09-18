965de575bf8c4a08fdbb01946e5e96abfd8a15bc
Sky <daniilanufriev0@gmail.com>
2026-09-18 16:33:28 +0300

    init

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { PrismaModule } from './prisma/prisma.module';
+import { BotModule } from './bot/bot.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.getOrThrow<string>('TELEGRAM_BOT_TOKEN'),
+      }),
+      inject: [ConfigService],
+    }),
+    BotModule,
+    UserModule,
+  ],
+})
+export class AppModule {}
